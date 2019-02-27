fe11b5be62475ed25b42d73da47494067b6af978
th3or14 <th3or14@gmail.com>
2019-02-27 20:24:06 +0200

    postincrement -> preincrement

--- sample/widget.cpp
+++ sample/widget.cpp
@@ -26,7 +26,7 @@ void Widget::on_write_video_clicked()
         QMessageBox::warning(this, "Status", "Can't open file for writing!", QMessageBox::Ok);
         return;
     }
-    for (int i = 0; i < 256; i++)
+    for (int i = 0; i < 256; ++i)
     {
         cv::Mat frame(frame_height, frame_width, CV_8UC3, cv::Scalar(i, 255 - i, 90));
         cv::putText(frame, "Hello, world!", cv::Size(20, 20), cv::FONT_HERSHEY_COMPLEX_SMALL, 1,
