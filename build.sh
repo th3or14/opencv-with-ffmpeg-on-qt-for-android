9598870df15e65c710df920bb9ffbd041f3bc9c0
th3or14 <th3or14@gmail.com>
2019-09-29 09:22:26 +0300

    NDK_PLATFORM_LEVEL=21

--- build.sh
+++ build.sh
@@ -2,7 +2,7 @@
 
 export NDK_PATH=$HOME/Android/android-ndk-r20
 export NDK_COMPILER_VERSION=4.9
-export NDK_PLATFORM_LEVEL=16
+export NDK_PLATFORM_LEVEL=21
 export HOST=linux-x86_64
 export ANDROID_SDK_ROOT=$HOME/Android/Sdk
 export CC=$NDK_PATH/toolchains/llvm/prebuilt/$HOST/bin/clang
