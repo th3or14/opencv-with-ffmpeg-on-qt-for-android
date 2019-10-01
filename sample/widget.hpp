27475f9b71b12e884cc3cd274906e516b61ddc58
th3or14 <37025359+th3or14@users.noreply.github.com>
2019-10-01 19:41:50 +0300

    remove extra space

--- sample/widget.hpp
+++ sample/widget.hpp
@@ -4,7 +4,7 @@
 #include <QWidget>
 #include <QMessageBox>
 #include <QStandardPaths>
- #include <QtAndroid>
+#include <QtAndroid>
 
 #include <opencv2/opencv.hpp>
 #include <opencv2/videoio/videoio_c.h>
